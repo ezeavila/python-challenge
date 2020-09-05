1e49918706255eed2e10c724608504349b5c8104
ezeavila <67862259+ezeavila@users.noreply.github.com>
2020-09-05 07:57:23 -0400

    Total Count Updated

--- PyPoll/Analysis/main.py
+++ PyPoll/Analysis/main.py
@@ -5,6 +5,7 @@ csvpath = os.path.join('..' , 'Resources', '02-Homework_03-Python_Instructions_P
 
 totalvotes = 0
 
+
 with open(csvpath) as pypollcsv:
     csvreader = csv.reader(pypollcsv, delimiter=',')
     print(csvreader)
@@ -12,14 +13,22 @@ with open(csvpath) as pypollcsv:
     print (f"CSV Header : {csv_header}")
 
     #totalvotes = totalvotes + 1
+
+    candidates = {}
     for row in csvreader:
         totalvotes += 1
+        if row[2] not in candidates:
+            candidates[row[2]] = 0
+        candidates[row[2]] = candidates[row[2]] + 1
+
+        final_list = [{'Candidate' : row, 'Total Votes' : candidates[row], 'Percentage %' : round(float(candidates[row]/totalvotes * 100),2)} for row in candidates]
+
 
 
 print("Election Results")
 print("___________________________________")
 print([f"Total Votes: {int(totalvotes)}"])
-
+print(final_list)
 
 
 output_file = os.path.join("outputpypoll.csv")
@@ -28,3 +37,4 @@ with open(output_file, "w") as summaryelection:
     writer = csv.writer(summaryelection)
     writer.writerow(["Election Results"])
     writer.writerow([f"Total Votes: {totalvotes}"])
+    writer.writerow([f"Results by Candidate: {final_list}"])
